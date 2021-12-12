15317ad89bb52de90e10c4e3fd197c2180c8ca26
SaintMiner <dinden290@gmail.com>
2021-12-12 17:32:23 +0200

    added language pick & stepper

--- src/App.vue
+++ src/App.vue
@@ -8,45 +8,106 @@
     </v-app-bar>
 
     <v-main>
-      <v-container fill-height>
-        <div v-if="isSuccess" class="mx-auto text-center green--text">
-          <v-icon style="font-size: 100pt;" color="green"> mdi-emoticon-happy-outline </v-icon>
-          <h1> {{ $t("Well done!") }} </h1>
-        </div>
-        <div v-else-if="isFailure" class="mx-auto text-center red--text">
-          <v-icon style="font-size: 100pt;" color="red"> mdi-emoticon-sad-outline </v-icon>
-          <h1> {{ $t("I am not satisfied...") }} </h1>
-        </div>
-        <v-card v-else
-          class="mx-auto my-auto"
-          elevation=""
-          max-width=""
-          tile
-          width="400px"
-        >
-          <v-list-item v-for="drink in drinks" :key="drink.value">
-            <v-list-item-content>
-              <v-checkbox
-                v-model="selected"
-                :label="$t(drink.label)"
-                :value="drink.value"
-                :disabled="isSendingDrinks"
-              ></v-checkbox>
-            </v-list-item-content>
-          </v-list-item>
-          <v-card-actions>
-            <v-btn
-              tile
-              color="info"
-              class="mx-auto"
-              @click="sendDrinks"
-              :loading="isSendingDrinks"
-            >
-              <v-icon left> mdi-send </v-icon>
-              {{ $t("Send") }}
-            </v-btn>
-          </v-card-actions>
-        </v-card>
+      <v-container>
+        <LanguagePicker class="mb-5" />
+        <v-stepper v-model="currentStep" width="800px" class="mx-auto my-auto">
+          <v-stepper-header>
+            <v-stepper-step :complete="currentStep > 1" step="1">
+              {{ $t("Did you drink today?") }}
+            </v-stepper-step>
+            <v-divider></v-divider>
+            <v-stepper-step :complete="currentStep > 2" step="2">
+              {{ $t("What did you drink today?") }}
+            </v-stepper-step>
+            <v-divider></v-divider>
+            <v-stepper-step :complete="currentStep == 3" step="3">
+              {{ $t("Result") }}
+            </v-stepper-step>
+          </v-stepper-header>
+          <v-stepper-items>
+            <v-stepper-content step="1">
+              <h3 class="text-center mb-4">
+                {{ $t("Did you drink today?") }}
+              </h3>
+              <v-card-actions>
+                <v-btn
+                  tile
+                  color="success"
+                  class="mx-auto"
+                  @click="currentStep++"
+                >
+                  <v-icon left> mdi-check </v-icon>
+                  {{ $t("Yes") }}
+                </v-btn>
+                <v-btn tile color="error" class="mx-auto" @click="didNotDrink">
+                  <v-icon left> mdi-close </v-icon>
+                  {{ $t("No") }}
+                </v-btn>
+              </v-card-actions>
+            </v-stepper-content>
+
+            <v-stepper-content step="2">
+              <h3 class="text-center mb-4">
+                {{ $t("What did you drink today?") }}
+              </h3>
+              <v-list>
+                <v-list-item v-for="drink in drinks" :key="drink.value">
+                  <v-list-item-content>
+                    <v-checkbox
+                      v-model="selected"
+                      :label="$t(drink.label)"
+                      :value="drink.value"
+                      :disabled="isSendingDrinks"
+                    ></v-checkbox>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+              <v-divider></v-divider>
+              <v-card-actions>
+                <v-btn
+                  tile
+                  color="primary"
+                  class="mx-auto"
+                  @click="sendDrinks"
+                  :loading="isSendingDrinks"
+                >
+                  <v-icon left> mdi-send </v-icon>
+                  {{ $t("Send") }}
+                </v-btn>
+              </v-card-actions>
+            </v-stepper-content>
+
+            <v-stepper-content step="3">
+              <div v-if="isSuccess" class="mx-auto text-center green--text">
+                <v-icon style="font-size: 100pt" color="success">
+                  mdi-emoticon-happy-outline
+                </v-icon>
+                <h1>{{ $t("Well done!") }}</h1>
+              </div>
+              <div v-if="isFailure" class="mx-auto text-center red--text">
+                <v-icon style="font-size: 100pt" color="error">
+                  mdi-emoticon-sad-outline
+                </v-icon>
+                <h1>{{ $t("I am not satisfied...") }}</h1>
+              </div>
+              <div
+                v-if="isNeedsDrink"
+                class="mx-auto text-center warning--text"
+              >
+                <v-icon style="font-size: 100pt" color="warning">
+                  mdi-emoticon-neutral-outline
+                </v-icon>
+                <h1>{{ $t("Go drink some water") }}</h1>
+              </div>
+              <v-card-actions>
+                <v-btn tile color="primary" class="mx-auto" @click="repeat">
+                  <v-icon left> mdi-repeat </v-icon>
+                  {{ $t("Repeat") }}
+                </v-btn>
+              </v-card-actions>
+            </v-stepper-content>
+          </v-stepper-items>
+        </v-stepper>
       </v-container>
     </v-main>
     <v-snackbar v-model="snackbar.show" :timeout="snackbar.time">
@@ -62,42 +123,31 @@
 </template>
 
 <script>
+import drinks from "./assets/drinks.json";
+import LanguagePicker from "./components/LanguagePicker.vue";
+
 export default {
   name: "App",
 
-  components: {},
+  components: {
+    LanguagePicker,
+  },
 
   data() {
     return {
-      drinks: [
-        {
-          label: "Water",
-          value: "water",
-        },
-        {
-          label: "Coffee",
-          value: "coffee",
-        },
-        {
-          label: "Latte",
-          value: "Latte",
-        },
-        {
-          label: "Tea",
-          value: "tea",
-        },
-        {
-          label: "Alcohol",
-          value: "alcohol",
-        },
-      ],
+      currentStep: 1,
+
+      drinks: drinks,
       selected: [],
-      isSendingDrinks: false,
+
       snackbar: {
         show: false,
         text: null,
         timeout: 1000,
       },
+
+      isNeedsDrink: false,
+      isSendingDrinks: false,
       isSuccess: false,
       isFailure: false,
     };
@@ -111,14 +161,24 @@ export default {
         this.isSuccess = false;
         this.isFailure = false;
         if (!this.selected.length) {
-          this.snackbar.text = this.$t("You died of dehydration");
+          this.snackbar.text = this.$t("You died of dehydration?");
           this.snackbar.show = true;
-        } else if (this.selected.includes('alcohol')) {
+        } else if (this.selected.includes("alcohol")) {
           this.isFailure = true;
         } else {
           this.isSuccess = true;
         }
+        this.currentStep++;
       }, 2000);
+    },
+
+    didNotDrink() {
+      this.isNeedsDrink = true;
+      this.currentStep = 3;
+    },
+
+    repeat() {
+      Object.assign(this.$data, this.$options.data());
     },
   },
 };
---
+++ src/components/LanguagePicker.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-card class="mx-auto my-auto" tile width="400px">
+    <v-list-item three-line>
+      <v-list-item-content>
+        <v-select
+          v-if="$i18n"
+          :items="langs"
+          :label="$t('Language')"
+          v-model="$i18n.locale"
+        ></v-select>
+      </v-list-item-content>
+    </v-list-item>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      langs: ["ru", "en"],
+    };
+  },
+};
+</script>
+
+<style></style>
